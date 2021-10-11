87b1e1c1cff3cb227c65e19b2ef1fa93ceea4634
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-10-11 08:14:09 +0400

    add  constructor to the Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,27 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post {
+
+  public function __construct(public $title, public $excerpt, public $date, public $body, public $slug) {
+
+    $this->title = $title;
+
+    $this->excerpt = $excerpt;
+
+    $this->date = $date;
+
+    $this->body = $body;
+
+    $this->slug = $slug;
+  }
+
+
+
   public static function find($slug) {
 
-    if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
-      throw new ModelNotFoundException();
-    }
+    $posts = static::all();
 
-    return cache()->remember("posts.{$slug}", 1200, function () use ($path) {
-      return file_get_contents($path);
-    });
+    return $posts->firstWhere('slug', $slug);
   }
 
   public static function all() {
-    $files =
-      File::files(resource_path("posts/"));
-
-    return array_map(function ($file) {
-      return $file->getContents();
-    }, $files);
+    return collect(File::files(resource_path("posts/")))
+      ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+      ->map(
+        fn ($document) =>
+        new Post(
+          $document->title,
+          $document->excerpt,
+          $document->date,
+          $document->body(),
+          $document->slug,
+        )
+      );
   }
 }
