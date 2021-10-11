c0b7205d5a67a8291962cafb2c41c4dffb879aaf
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-10-11 09:18:53 +0400

    use layout blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>My Blog</title>
-  <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section()
   <article>
-    <h1><?= $post->title; ?></h1>
+    <h1>
+      {{ $post->title }}
+    </h1>
     <div>
-      <?= $post->body; ?>
+      {!! $post->body !!}
     </div>
   </article>
 
   <a href="/" >Go Back</a>
-
- 
-</body>
-</html>
+ @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>My Blog</title>
-  <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-  <?php foreach ($posts as $post) : ?>
+@extends('layout')
+
+@section("content")
+
+  @foreach ($posts as $post)
   
   <article>
     <h1>
-      <a href="/posts/<?= $post->slug; ?>">
-        <?= $post->title; ?>
+      <a href="/posts/{{ $post->slug }}">
+        {{ $post->title }}
       </a>
     </h1>
     <div>
-      <?= $post->excerpt; ?>
+      {{ $post->excerpt }}
     </div>
   </article>
 
-  <?php endforeach; ?>
+  @endforeach
 
-  
-</body>
-</html>
+@endsection
