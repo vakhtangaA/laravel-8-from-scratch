c8509cc3ec8870d843d78932248c77c405456683
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-10-15 17:58:40 +0400

    refactor: remove category route
    implement category search by uri queries, instead of route paths

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,23 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Models\Category;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
 
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::all()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(3),
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use App\Models\User;
-use App\Models\Category;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,20 +17,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
+Route::get('/{so}', [PostController::class, 'index'])->name('home');
+
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category->name,
-        'categories' => Category::all()
-
-    ]);
-})->name('category');
-
 Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
+    return view('posts.index', [
         'posts' => $author->posts,
-        'categories' => Category::all()
     ]);
 });
