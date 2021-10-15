6447ad16cb98c59cb5b53503482ef9fbc123253a
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-10-15 08:32:01 +0400

    feat: add search functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,14 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
