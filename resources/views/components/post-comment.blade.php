405b916f0c3e3e56271a23388f22839424df6038
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-10-19 14:33:56 +0400

    refactor: change date format and avatar id

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -3,7 +3,7 @@
 >
   <div class="flex-shrink-0">
     <img
-      src="https://i.pravatar.cc/60?u={{ $comment->id }}"
+      src="https://i.pravatar.cc/60?u={{ $comment->user_id }}"
       alt="avatar"
       width="60px"
       height="60px"
@@ -14,7 +14,7 @@
     <header class="mb-4">
       <h3 class="font-bold">{{ $comment->author->username }}</h3>
       <p class="text-xs">
-        Posted <time>{{ $comment->created_at }}</time>
+        Posted <time>{{ $comment->created_at->format('Y d') }}</time>
       </p>
     </header>
     <p>
