f63767616ef3422fa5a5fbc76ab27056ca4f7265
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-12-08 16:58:35 +0400

    fix: extract validation logic

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
 use App\Models\Post;
 
 class PostCommentsController extends Controller
 {
-    public function store(Post $post)
-    {
-        request()->validate([
-            'body' => 'required'
-        ]);
+	public function store(Post $post, StoreCommentRequest $request)
+	{
+		$validated = $request->validated();
 
-        $post->comments()->create([
-            'user_id' => request()->user()->id,
-            'body' => request('body')
-        ]);
+		$post->comments()->create([
+			'user_id' => $request->user()->id,
+			'body'    => $validated['body'],
+		]);
 
-        return back();
-    }
+		return back();
+	}
 }
