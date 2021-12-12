f1bb481b23cbdb9014de6193106ae0b979f99f19
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-12-12 11:18:47 +0400

    fix: use asset function

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,18 +1,18 @@
 <article
          {{ $attributes->merge([
              'class' => '
-                     transition-colors
-                     duration-300
-                     hover:bg-gray-100
-                     border border-black border-opacity-0
-                     hover:border-opacity-5
-                     rounded-xl
-                     ',
+                              transition-colors
+                              duration-300
+                              hover:bg-gray-100
+                              border border-black border-opacity-0
+                              hover:border-opacity-5
+                              rounded-xl
+                              ',
          ]) }}>
   <div class="py-6 px-5">
 
     <div>
-      <img src="{{ $post->thumbnail === null ? '/images/illustration-3.png' : asset('storage/' . $post->thumbnail) }}"
+      <img src="{{ $post->thumbnail === null ? asset('images/illustration-3.png') : asset('storage/' . $post->thumbnail) }}"
            alt="Blog Post illustration"
            class="rounded-xl" />
     </div>
